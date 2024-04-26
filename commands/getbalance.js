e48c1d2ab5f14d336a31fceb4eb22159facc9549
Mateo DM <104037425+mateodm@users.noreply.github.com>
2024-04-26 04:01:23 -0300

    fix

--- commands/getbalance.js
+++ commands/getbalance.js
@@ -17,7 +17,6 @@ module.exports = {
                 .setRequired(false)
         ),
     execute: async (interaction) => {
-        interaction.deferReply();
         let account = interaction.options.getString("account")
         let token = interaction.options.getString("token")
         if (account && token) {
@@ -33,10 +32,10 @@ module.exports = {
                     PendingUnstake: filter.pendingUnstake
                 }))
                 let table = generateTable(filterData)
-                interaction.editReply(`\`\`\`\n${table}\n\`\`\``);
+                interaction.reply(`\`\`\`\n${table}\n\`\`\``);
             }
             else {
-                await interaction.editReply("The user entered does not exist or params invalid. Please enter the token in UPPERCASE")
+                await interaction.reply("The user entered does not exist or params invalid. Please enter the token in UPPERCASE")
             }
         }
         else if (account) {
@@ -45,24 +44,17 @@ module.exports = {
             let result = response.data.result
             let balance = []
             if (result.length > 0) {
-                let tokenInterested = ["STARBITS", "STARPRO", "ZING", "SWAP.HIVE", "SWAP.SWIFT", "DEC", "SPS", "SWAP.MATIC", "SWAP.USDT", "SWAP.HBD", "BEE", "BUDS", "HKWATER", "MOTA", "PIZZA", "SPT", "SWAP.WAX", "KOD", "HUESO", "COLONY", "SCRIPT", "PART", "VALOR", "ORIGINS", "SCRAP", "FORGE", "STELLARUM", "HIQS", "PXL", "SIM", "CCD", "WOO", "CROP"];
-
+                let tokenInterested = ["STARBITS", "STARPRO", "SWAP.HIVE", "ZING", "ORIGINS", "STELLARUM", "SIM", "SCRAP", "WOO",  "SWAP.SWIFT", "DEC", "SPS", "SWAP.MATIC", "SWAP.USDT", "SWAP.HBD", "BEE", "BUDS", "HKWATER", "MOTA", "PIZZA", "SPT", "SWAP.WAX", "KOD", "HUESO"]
                 const filterResults = result.filter(filter => tokenInterested.includes(filter.symbol));
                 filterResults.map(filter => {
-                    try {
-                        let symbol = { [filter.symbol]: filter.balance }
-                        balance.push(symbol)
-                    }
-                    catch(e) {
-                        console.log(e)
-                    }
-
+                    let symbol = { [filter.symbol]: filter.balance }
+                    balance.push(symbol)
                 })
                 let table = generateTableBalance(balance, account)
-                interaction.editReply(`\`\`\`\n${table}\n\`\`\``);
+                interaction.reply(`\`\`\`\n${table}\n\`\`\``);
             }
             else {
-                await interaction.editReply("The user entered does not exist or the parameters are incorrectly set")
+                await interaction.reply("The user entered does not exist or the parameters are incorrectly set")
             }
         }
     },
