dc65ae3a8fdc628c2a109b8bd610338034ed5d68
Mateo DM <mateodm03@gmail.com>
2024-12-29 04:50:20 -0300

    giveaway-fix

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,4 +1,3 @@
-
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 const hive = require("@hiveio/hive-js")
 
@@ -8,7 +7,7 @@ module.exports = {
         .setDescription("Get a winner of the comments")
         .addStringOption(option =>
             option.setName("link")
-                .setDescription("link of the post to givewaway")
+                .setDescription("Link of the post to giveaway")
                 .setRequired(true)
         ),
     execute: async (interaction) => {
@@ -20,32 +19,51 @@ module.exports = {
                 const usuario = matches[1];
                 const permlink = matches[2];
                 await hive.api.getContentReplies(usuario, permlink, function (err, result) {
-                    let participants = []
-                    let bots = ["indiaunited", "lolzbot", "hug.bot", "hive-112281", "pgm-curator", "steem-plus", "hivegifbot", "germanbot", "curation-cartel", "youarealive", "hivebuzz", "lolz.pgm", "threespeak", "bdvoter.cur", "zottonetoken", "splinterboost", "ecency", "pizzabot", "discovery-it", "risingstargame", "steem-ua", "hk-gifts", "tipu", "poshtoken", "actifit", "hiq.smartbot", "india-leo", "upvoteturtle", "hivebits", "thepimpdistrict", "hiq.smartbot", "terraboost", "meme.bot", "xyz.store", "stemsocial", "bbhbot", "pinmapple", "luvshares"]
+                    if (err || !result || result.length === 0) {
+                        return interaction.reply("No comments found or an error occurred.");
+                    }
 
-                    let participantsUser = ""
+                    let participants = [];
+                    let bots = ["indiaunited", "lolzbot", "hug.bot", "hive-112281", "pgm-curator", "steem-plus", "hivegifbot", "germanbot", "curation-cartel", "youarealive", "hivebuzz", "lolz.pgm", "threespeak", "bdvoter.cur", "zottonetoken", "splinterboost", "ecency", "pizzabot", "discovery-it", "risingstargame", "steem-ua", "hk-gifts", "tipu", "poshtoken", "actifit", "hiq.smartbot", "india-leo", "upvoteturtle", "hivebits", "thepimpdistrict", "hiq.smartbot", "terraboost", "meme.bot", "xyz.store", "stemsocial", "bbhbot", "pinmapple", "luvshares"];
+                    let participantsUser = "";
                     result.forEach(participant => {
-                        let find = bots.find(user => user === participant.author)
+                        let find = bots.find(user => user === participant.author);
                         if (!find) {
-                            participants.push({ author: participant.author, comment: participant.body, url: `https://peakd.com${participant.url}`, title: participant.root_title })
-                            participantsUser = participantsUser + "@" + participant.author + ", "
+                            participants.push({ author: participant.author, comment: participant.body, url: `https://peakd.com${participant.url}`, title: participant.root_title });
+                            participantsUser += `@${participant.author}, `;
                         }
-                    })
-                    let numberParticipants = participants.length
-                    let ramdom = Math.floor((Math.random() * numberParticipants))
-                    let winner = participants[ramdom]
-                    let comment = winner.comment.slice(0, 120)
-                    let image = ""
+                    });
+
+                    if (participants.length === 0) {
+                        return interaction.reply("No valid participants found.");
+                    }
+
+                    let numberParticipants = participants.length;
+                    let random = Math.floor((Math.random() * numberParticipants));
+                    let winner = participants[random];
+                    let comment = winner.comment ? winner.comment.slice(0, 120) : "No comment available.";
+                    let image = "https://i.imgur.com/EEd2bot.jpeg"; // Default image
+
+                    // Attempt to fetch profile image, fallback to default if unsuccessful
                     hive.api.callAsync('condenser_api.get_accounts', [[winner.author]])
                         .then((res) => {
-                            let image = "https://i.imgur.com/EEd2bot.jpeg"
-                            if (res[0].posting_json_metadata !== "") {
-                                let profile = JSON.parse(res[0].posting_json_metadata)
-                                image = profile.profile.profile_image || "https://i.imgur.com/EEd2bot.jpeg"
+                            if (res && res[0]?.posting_json_metadata) {
+                                try {
+                                    let profile = JSON.parse(res[0].posting_json_metadata);
+                                    image = profile?.profile?.profile_image || image;
+                                } catch (e) {
+                                    console.error("Error parsing profile metadata:", e);
+                                }
                             }
+                        })
+                        .catch(err => {
+                            console.error("Error fetching account data:", err);
+                        })
+                        .finally(() => {
+                            // Always continue with the giveaway
                             const embed = new EmbedBuilder()
                                 .setColor("#FF0000")
-                                .setTitle(winner.title)
+                                .setTitle(winner.title || "Unknown Title")
                                 .setURL(winner.url)
                                 .setAuthor({ name: "devilbot", iconURL: "https://i.imgur.com/S1LpJ8G.png", url: 'https://discord.js.org' })
                                 .setDescription(`# **The winner of this giveaway is**:
@@ -59,15 +77,14 @@ module.exports = {
                                 .setFooter({ text: 'devilbot includes rs-stats, giveaways, get post info and more', iconURL: 'https://i.imgur.com/S1LpJ8G.png' });
 
                             interaction.reply({ embeds: [embed] });
-                        })
-
+                        });
                 });
             } else {
-                interaction.reply("Invalid link")
+                interaction.reply("Invalid link");
             }
         } catch (e) {
-            console.log(e)
+            console.error("Unexpected error:", e);
+            interaction.reply("An unexpected error occurred.");
         }
-
     },
 }
--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const { Client, GatewayIntentBits, Collection, REST, Routes, Events } = require("discord.js");
 const fs = require("fs");
-const path = require("path");
-const axios = require("axios")
 const express = require("express")
-const { buyOrder } = require("./utils/buyOrder.js")
-const dhive = require("@hiveio/dhive")
-const { ethers } = require("ethers")
 require('dotenv').config();
 
 
 const app = express()
 const ready = () => console.log("Server running in port " + 3031)
-const httpServer = app.listen(3031, ready)
+app.listen(3031, ready)
 
 const client = new Client({
     intents: [
@@ -21,6 +16,8 @@ const client = new Client({
         GatewayIntentBits.GuildMembers,
     ]
 });
+
+
 
 client.commands = new Collection()
 
@@ -63,77 +60,3 @@ client.on(Events.InteractionCreate, async (interaction) => {
 })
 
 client.login(process.env.login)
-
-const CONTRACT_ADRESS = "0xe9E6b372a8bB30cf95387E10F66b9cA193416e5b"
-
-
-async function starproToken() {
-    try {
-
-    }
-    catch (e) {
-        console.log(e)
-    }
-}
-
-starproToken()
-
-async function addCardsToDB() {
-    const filePath = "cards.json"
-    let offset = 15418000
-    let length = 1000
-    let data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    let count = 0
-    do {
-        let result = await axios.post("https://herpc.dtools.dev/contracts", { "jsonrpc": "2.0", "id": 10, "method": "find", "params": { "contract": "nft", "table": "STARinstances", "query": {}, "limit": 1000, "offset": offset, "indexes": ["account"] } })
-        offset += 1000
-        length = result.data.result.length
-        let allCards = result.data.result
-        console.log(offset)
-        allCards.forEach(cards => {
-            let propertiesType = cards.properties.type
-            if (!data.hasOwnProperty(propertiesType)) {
-                let existingObject = data.find(obj => obj.name === propertiesType);
-                if (existingObject) {
-                    count++
-                    return;
-                }
-                else {
-                    let fans = "0"
-                    let luck = "0"
-                    let skill = "0"
-                    let im = "0"
-                    if (cards.properties.stats) {
-                        let stats = cards.properties.stats.split(",")
-                        fans = stats[0]
-                        luck = stats[1]
-                        skill = stats[2]
-                        im = stats[3]
-                    }
-                    const newObject = {
-                        name: cards.properties.type,
-                        class: cards.properties.class,
-                        fans: fans,
-                        luck: luck,
-                        skill: skill,
-                        im: im,
-                    }
-                    data.push(newObject)
-                    console.log(data.length, count)
-                }
-            }
-        })
-    }
-    while (length === 1000)
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 4))
-}
-
-/* async function start() {
-    apiKey= "cqt_rQgMJddtFYd7KyX9hyv7rk3ywFXm"
-    const poolAddress = "0x02ba675fec5a601d206e65a8d6f23b2fe694a61c"
-    const chainId= 137
-    const endpoint = `https://api.covalenthq.com/v1/${chainId}/xy=k/uniswap_v3/pools/address/${poolAddress}/transactions/?key=${apiKey}`;
-    let response = await axios.get(endpoint)
-}
-
-start() */
